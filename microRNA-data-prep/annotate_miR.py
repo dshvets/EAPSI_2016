5fe65bf42b6f40bd0a7904ba4059f711a7449a35
Dahlia Shvets <dahliashvets@gmail.com>
2016-07-06 11:16:31 +0800

    add trigger word

--- microRNA-data-prep/annotate_miR.py
+++ microRNA-data-prep/annotate_miR.py
@@ -1,12 +1,12 @@
-import re 
+import re
 
-#Script for splitting inputfile into 200 line resulting files, with one file representing a document of text. Final files include start and stop of miRNA within the document. 
+#Script for splitting inputfile into 200 line resulting files, with one file representing a document of text. Final files include start and stop of miRNA within the document.
 
-#Reg-ex function to search for miRNA in the sentence and obtain the start and end position
-#Input x is the sentence, y is an array including the miRNA of interest
+#Regex function to search for miRNA/trigger word in the sentence and obtain the start and end position
+#Input x is the sentence, y is an array including the miRNA(or trigger word) of interest
 def getPosition(x,y):
     y=list(set(y)) #Get only unique miRNA from list
-    dict = {}   #dictionary to store miRNA name and positions within sentence 
+    dict = {}   #dictionary to store miRNA name and positions within sentence
     for i in y:
         pos_array=[]
         regex = r"\b"+re.escape(i)+r"\b"
@@ -15,17 +15,30 @@ def getPosition(x,y):
             e=match.end() -1
             start_end=[s,e]
             pos_array.append(start_end)
-        dict[i] = pos_array 
-    return dict 
+        dict[i] = pos_array
+    return dict
 
-#Open original file for reading 
-#If file is .XLSX, convert it to .Txt (tab-separated file)
-#Check that set list has correct new line characters in original file 
+
+#Regex function to search for all instances of disease position in sentence
+#x is sentence, y is disease name
+def getDisease(x,y):
+    disease_array=[]
+    for match in re.finditer(y,x):
+        s=match.start()
+        e=match.end() -1
+        se = [s,e]
+        disease_array.append(se)
+    return disease_array
+
+
+#Open original file for reading
+#If file is .XLSX, convert it to .txt (tab-separated file)
+#Vim into tab delimited file and type :%s/\r/\r/g in order to check that set list has correct new line characters in original file
 orig_file = open("practice.txt","r")
 
-count = 1 #initialize count of every 200 line document 
-docu_length = 0 #initialize length of 200 line document 
-file_number = 1
+count = 1 #initialize count of every 200 line document
+docu_length = 0 #initialize length of 200 line document
+file_number = 1   #initialize first file number
 fileName = "file"+str(file_number)
 toWrite = open(fileName,'w')
 firstLine = "Row#\tStart\tEnd\tType\tName\n"
@@ -38,8 +51,11 @@ for line in orig_file:
         sentence = data[1]
         mrna=data[2]
         mir=mrna.split("|")
-        if(count < 5):      #loop through every 200 lines at a time 
-            this_length = len(line)
+        trig=data[3]
+        trigger=trig.split("|")
+        disease = data[4]
+        if(count < 5):      #loop through every 200 lines at a time
+            this_length = len(sentence)
             result = getPosition(sentence,mir)
             for key in result:
                 array_position = result[key]
@@ -48,12 +64,23 @@ for line in orig_file:
                     end = docu_length + pos[1]
                     newLine = row,start,end,"miRNA",key
                     toWrite.write('\t'.join(map(str,newLine))+'\n')
+
+            trig_result = getPosition(sentence,trigger)
+
+            for trig_key in trig_result:
+                trig_value = trig_result[trig_key]
+                for trig_pos in trig_value:
+                    start = docu_length + trig_pos[0]
+                    end = docu_length + trig_pos[1]
+                    newLine = row,start,end,"Trigger Word",trig_key
+                    toWrite.write('\t'.join(map(str,newLine))+'\n')
+
             docu_length = docu_length + this_length     #update docu_length
             count +=1
         else:
             toWrite.close()
             docu_length = 0 #Reset at zero for next 200 lines to begin
-            this_length = len(line)
+            this_length = len(sentence)
             count =1
             file_number += 1
             fileName = "file"+str(file_number)
@@ -67,10 +94,17 @@ for line in orig_file:
                     end = docu_length + pos[1]
                     newLine = row,start,end,"miRNA",key
                     toWrite.write('\t'.join(map(str,newLine))+'\n')
+
+            trig_result = getPosition(sentence,trigger)
+            for trig_key in trig_result:
+                trig_value = trig_result[trig_key]
+                for trig_pos in trig_value:
+                    start = docu_length + trig_pos[0]
+                    end = docu_length + trig_pos[1]
+                    newLine = row,start,end,"Trigger Word",trig_key
+                    toWrite.write('\t'.join(map(str,newLine))+'\n')
+
+
+
             docu_length = docu_length + this_length
             count +=1
-
-
-
-
-
