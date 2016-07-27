81ee323be4b53705dbf4d34b7630739470c5efdb
Dahlia Shvets <dahliashvets@gmail.com>
2016-07-27 14:28:56 +0800

    update before change

--- NLP_Abstracts/Sentence_Classification/disease_lookup.py
+++ NLP_Abstracts/Sentence_Classification/disease_lookup.py
@@ -38,24 +38,34 @@ def removeTumor(x):
 
 
 classifiedFile = open("classified_data.txt",'r')
+classifiedDisease = open('classify_disease_data.txt','w')
 
 for line in classifiedFile:
     line = line.split('\t')
     pmid = line[0]      #use PubMed ID to search disease type with PubTator API
     sent = line[1]
     groupType = line[2]
-    url = "http://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/Disease/"+pmid+"/PubTator/"
-    r = requests.get(url)
-    text = r.text
+    groupType = groupType.rstrip('\n')
     try:
+        url = "http://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/Disease/"+pmid+"/PubTator/"
+        r = requests.get(url)
+        text = r.text
         disease = getDisease(text)
         noCancer = removeCancer(disease)
         noDisease = removeDisease(noCancer)
-        noTumor = removeTumor(noDisease)
-
-        
+        finalDisease = removeTumor(noDisease)
+        in_sent = []    #save disease names that are in current sentence
+        for x in finalDisease:
+            diseaseSearch = bool(re.search(x,sent))
+            if diseaseSearch == True:
+                in_sent.append(x)
+                if len(in_sent) >= 1:
+                    for i in in_sent:
+                        newLine = pmid+'\t'+sent+'\t'+groupType+'\t'+i+'\n'
+                        classifiedDisease.write(newLine)
     except:
         pass
 
 
 classifiedFile.close()
+classifiedDisease.close()
